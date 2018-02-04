e11a2367849d0e846e3fcc8d850003675066f2bd
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-02-04 15:26:09 +0800

    write Main4.hs

--- src/Main4.hs
+++ src/Main4.hs
@@ -1,14 +1,17 @@
 module Main4 where
 
 import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Except
 import Control.Monad
 import Control.Monad.Error.Class
 
 type Request = String
 type Response = String
 
-type Application = Request -> ActionT
-type ActionT = Either ActionError Response
+type Application = Request -> Response
+type ActionT = ExceptT ActionError (Reader Request) Response
 type ActionError = String
 
 type Route = Application -> Application
@@ -20,14 +23,16 @@ type AppStateT = State AppState
 constructResponse :: String -> String -> String
 constructResponse req msg = unwords ["Request:", req, "\nResponse:", msg]
 
-routeHandler1 :: Request -> ActionT
-routeHandler1 request = return $ constructResponse request "Hello from Route 1"
+routeHandler1 :: ActionT
+routeHandler1 = do
+  request <- lift ask
+  return $ constructResponse request "Hello from Route 1"
 
-routeHandler2 :: Request -> ActionT
-routeHandler2 request = throwError "Error in Route 2"
+routeHandler2 :: ActionT
+routeHandler2 = throwError "Error in Route 2"
 
-defaultRoute :: Request -> ActionT
-defaultRoute request = return $ constructResponse request "Hello from the DEFAULT route"
+defaultRoute :: Application
+defaultRoute request = "Hello from the DEFAULT route"
 
 myApp :: AppStateT ()
 myApp = do
@@ -38,24 +43,29 @@ main :: IO ()
 main = myServer myApp
 
 -- framework methods
-addRoute :: String -> Application -> AppStateT ()
+addRoute :: String -> ActionT -> AppStateT ()
 addRoute pat mf = modify $ \s -> addRoute' (route pat mf) s
 
 addRoute' :: Route -> AppState -> AppState
 addRoute' mf s@AppState {routes = mw} = s {routes = mf:mw}
 
-route :: String -> (Request -> ActionT) -> Route
+route :: String -> ActionT -> Route
 route pat mw mw1 input_string =
   let tryNext = mw1 input_string in
-  if pat == input_string
-  then
-    mw input_string
-  else
-    tryNext
+    if pat == input_string
+      then
+        runAction mw input_string
+      else
+        tryNext
 
-runMyApp :: (Request -> ActionT) -> AppState -> Request -> ActionT
+runMyApp :: Application -> AppState -> Application
 runMyApp def app_state =
   foldl (flip ($)) def (routes app_state)
+
+runAction :: ActionT -> Request -> Response
+runAction action request = either id id
+                           $ flip runReader request
+                           $ runExceptT action
 
 userInputLoop app_state = do
   putStrLn "Awaiting requests..."
@@ -63,9 +73,8 @@ userInputLoop app_state = do
 
   unless (request == "q") $ do
     let response = runMyApp defaultRoute app_state request
-    case response of
-      Right x -> putStrLn x
-      Left e -> putStrLn $ "Error: " ++ e
+    putStrLn response
+    putStrLn "---"
     userInputLoop app_state
 
 myServer :: AppStateT () -> IO ()
