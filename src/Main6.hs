fd738e50def34fe0cec97ae1e82c48fa6e669005
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-02-05 16:04:34 +0800

    add either to Main6

--- src/Main6.hs
+++ src/Main6.hs
@@ -65,10 +65,12 @@ runMyApp defHandler appState =
   foldl (flip ($)) defHandler (routes appState)
 
 runAction :: ActionT () -> Request -> IO Response
-runAction action request = flip execStateT ""
-                           $ flip runReaderT request
-                           $ runExceptT
-                           $ action `catchError` errorHandler
+runAction action request = do
+  (a,s) <- flip runStateT ""
+           $ flip runReaderT request
+           $ runExceptT
+           $ action `catchError` errorHandler
+  return $ either (const "Error") (const s) a
 
 errorHandler :: ActionError -> ActionT ()
 errorHandler err = lift . lift $ modify (const $ "Oops: " ++ err)
